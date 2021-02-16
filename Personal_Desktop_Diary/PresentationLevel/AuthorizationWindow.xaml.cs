c33f59f0103658b0b44f54434edba15b9d5d3cc1
Yaroslav Kolisnyk <kolisnukyaroslav77@gmail.com>
2021-02-17 00:33:05 +0200

    Added authorization and main window

---
+++ Personal_Desktop_Diary/PresentationLevel/AuthorizationWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLibrary.Models;
+using DataAccessLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLevel
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        private UnitOfWork _unitOfWork;
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+            _unitOfWork = new UnitOfWork();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            User user = _unitOfWork.Users.GetSingleItem(u => u.UserName == UsernameTextbox.Text &&
+            u.Password == Password_box.Password);
+
+            if (user != null)
+            {                  
+                MainWindow mainWindow = new MainWindow(user);                
+                mainWindow.Show();
+                Close();
+            }
+            else
+                MessageBox.Show("User doesnt exist");
+        }
+    }
+}
--- Personal_Desktop_Diary/PresentationLevel/MainWindow.xaml.cs
+++ Personal_Desktop_Diary/PresentationLevel/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace PresentationLevel
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
+        private User _user;
+        public MainWindow(User user)
+        {           
             InitializeComponent();
+            _user = user;
+            welcomeTextblock.Text += $" {user.Name}  {user.Surname}";
         }
     }
 }
